c5d734f92a19d279834f0c321e8f5d09e4c69e63
Toshiyasu Hachiya <hachiya.t@technoface.co.jp>
2013-12-06 00:27:01 +0900

    インデントの修正

--- pystone.scm
+++ pystone.scm
@@ -4,67 +4,67 @@
 
 (define Proc5
   (lambda ()
-	'()
+    '()
   )
 )
 
 (define Proc4
   (lambda ()
-	'()
+    '()
   )
 )
 
 (define Proc0
   (lambda (loops)
-	(let ((starttime) (nulltime) (benchtime) (loopsPerBenchtime))
-	  (set! starttime (unix-time))
-	  (let loop ((i loops))
-		(if (> i 1)
-		  (loop (- i 1))
-		)
+    (let ((starttime) (nulltime) (benchtime) (loopsPerBenchtime))
+      (set! starttime (unix-time))
+      (let loop ((i loops))
+        (if (> i 1)
+          (loop (- i 1))
+        )
       )
-	  (set! nulltime (- (unix-time) starttime))
-	  
-	  ;PtrGlb
-	  
-	  (set! starttime (unix-time))
-	  (let loop ((i loops))
-		(if (> i 1)
-		  (let ((IntLoc1 2) (IntLoc2 3) (String2Loc "DHRYSTONE PROGRAM, 2'ND STRING")) 
-			(Proc5)
-			(Proc4)
-		    (loop (- i 1))
-		  )
-		)
+      (set! nulltime (- (unix-time) starttime))
+
+    ;PtrGlb
+
+    (set! starttime (unix-time))
+    (let loop ((i loops))
+      (if (> i 1)
+        (let ((IntLoc1 2) (IntLoc2 3) (String2Loc "DHRYSTONE PROGRAM, 2'ND STRING")) 
+            (Proc5)
+            (Proc4)
+            (loop (- i 1))
+        )
       )
+    )
 
-	  (set! benchtime (- (- (unix-time) starttime) nulltime))
+    (set! benchtime (- (- (unix-time) starttime) nulltime))
 
-	  (if (= benchtime 0)
-		(set! loopsPerBenchtime 0)
-		(set! loopsPerBenchtime (/ loops benchtime))
-	  )
+    (if (= benchtime 0)
+      (set! loopsPerBenchtime 0)
+      (set! loopsPerBenchtime (/ loops benchtime))
+    )
 
-	  (cons benchtime loopsPerBenchtime)
-	)
+    (cons benchtime loopsPerBenchtime)
+    )
   )
 )
 
 (define pystones
   (lambda (loops)
-	(Proc0 loops)
+    (Proc0 loops)
   )
 )
 
 (define main
   (lambda (loops)
-	(let ((result (pystones loops)))
-	  (display (string-append
-		"Pystone.scm(" __version__ ") time for "
-		(number->string loops) " passes = " (number->string (car result)) "\n"
-		"This machine benchmarks at " (number->string (cdr result)) " pystones/second\n"
-	  ))
-	)
+    (let ((result (pystones loops)))
+      (display (string-append
+        "Pystone.scm(" __version__ ") time for "
+        (number->string loops) " passes = " (number->string (car result)) "\n"
+        "This machine benchmarks at " (number->string (cdr result)) " pystones/second\n"
+      ))
+    )
   )
 )
 
