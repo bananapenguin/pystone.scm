e006cc6e8f46b3b488a2c8819bb5053fd3a03430
Toshiyasu Hachiya <hachiya.t@technoface.co.jp>
2013-12-05 18:03:35 +0900

    benchtimeの取得ができるように

--- pystone.scm
+++ pystone.scm
@@ -1,6 +1,52 @@
+(require 'unix)
+
+(define __version__ "0.1")
+
+(define Proc5
+  (lambda ()
+	'()
+  )
+)
+
+(define Proc4
+  (lambda ()
+	'()
+  )
+)
+
 (define Proc0
   (lambda (loops)
-	'()
+	(let ((starttime) (nulltime) (benchtime) (loopsPerBenchtime))
+	  (set! starttime (unix-time))
+	  (let loop ((i loops))
+		(if (> i 1)
+		  (loop (- i 1))
+		)
+      )
+	  (set! nulltime (- (unix-time) starttime))
+	  
+	  ;PtrGlb
+	  
+	  (set! starttime (unix-time))
+	  (let loop ((i loops))
+		(if (> i 1)
+		  (let ((IntLoc1 2) (IntLoc2 3) (String2Loc "DHRYSTONE PROGRAM, 2'ND STRING")) 
+			(Proc5)
+			(Proc4)
+		    (loop (- i 1))
+		  )
+		)
+      )
+
+	  (set! benchtime (- (- (unix-time) starttime) nulltime))
+
+	  (if (= benchtime 0)
+		(set! loopsPerBenchtime 0)
+		(set! loopsPerBenchtime (/ loops benchtime))
+	  )
+
+	  (cons benchtime loopsPerBenchtime)
+	)
   )
 )
 
@@ -12,9 +58,13 @@
 
 (define main
   (lambda (loops)
-	(pystones loops)
-	(display (string-append "Pystone.scm " "result\n"))
-	(display "This machine benchmarks at * pystones/second\n")
+	(let ((result (pystones loops)))
+	  (display (string-append
+		"Pystone.scm(" __version__ ") time for "
+		(number->string loops) " passes = " (number->string (car result)) "\n"
+		"This machine benchmarks at " (number->string (cdr result)) " pystones/second\n"
+	  ))
+	)
   )
 )
 
